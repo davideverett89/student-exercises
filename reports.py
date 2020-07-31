a867c1e37f222cd1848593cd60f59b1d07560aa4
David Everett <davideverett1989@gmail.com>
2020-07-31 15:12:10 -0500

    Chapter 3 read-through complete.

---
+++ reports.py
@@ -0,0 +1,40 @@
+import sqlite3
+from student import Student
+
+
+class StudentExerciseReports():
+
+    """Methods for reports on the Student Exercises database"""
+
+    def __init__(self):
+        self.db_path = "/Users/davideverett/workspace/python/StudentExercises/studentexercises.db"
+
+    def all_students(self):
+
+        """Retrieve all students with the cohort name"""
+
+        with sqlite3.connect(self.db_path) as conn:
+            conn.row_factory = lambda cursor, row: Student(row[1], row[2], row[3], row[5])
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select s.id,
+                s.first_name,
+                s.last_name,
+                s.slack_handle,
+                s.cohort_id,
+                c.name
+            from students s
+            join cohorts c on s.cohort_id = c.id
+            order by s.cohort_id
+            """)
+
+            all_students = db_cursor.fetchall()
+
+            # for student in all_students:
+            #     print(student)
+
+            [print(s) for s in all_students]
+
+reports = StudentExerciseReports()
+reports.all_students()
